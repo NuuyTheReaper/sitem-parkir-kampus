04225cdc075f74caf4ec4e523042d9677ba224fa
NuuyTheReaper <wisnusw142@gmail.com>
2026-06-13 08:47:18 +0700

    config: update backend URLs to production and adjust Android configurations

--- frontend/lib/core/constants.dart
+++ frontend/lib/core/constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
-  static const String baseUrl = 'http://127.0.0.1:8000/api/';
-  static const String uploadBaseUrl = 'http://127.0.0.1:8000';
-  static const String wsUrl = 'ws://127.0.0.1:8000/api/gate/monitor/live';
+  static const String baseUrl = 'https://parkirkampus.my.id/api/';
+  static const String uploadBaseUrl = 'https://parkirkampus.my.id';
+  static const String wsUrl = 'wss://parkirkampus.my.id/api/gate/monitor/live';
   static const String wsNotifUrl =
-      'ws://127.0.0.1:8000/api/gate/petugas/notifications';
+      'wss://parkirkampus.my.id/api/gate/petugas/notifications';
 }
